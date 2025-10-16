99d25476e0824bb6c2e00dbdd20b33e041e61113
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-10-16 20:20:29 +0000

    Tabella Studente

---
+++ biblioteca.sql
@@ -0,0 +1,7 @@
+-- Creo le Tabelle
+Create Table Studente(
+    Matricola Int Primary Key,
+    Nome Varchar(50) Not Null,
+    Cognome Varchar(50) Not Null,
+    Corso_di_Laurea Varchar(70) Not Null
+);
