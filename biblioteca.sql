a0146d940d3b6951af4633891def47bb399e6921
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-10-16 20:36:41 +0000

    Tutte le Tabelle

--- biblioteca.sql
+++ biblioteca.sql
@@ -5,3 +5,21 @@ Create Table Studente(
     Cognome Varchar(50) Not Null,
     Corso_di_Laurea Varchar(70) Not Null
 );
+
+Create Table Prestito(
+    Id_Prestito Int Primary Key,
+    Inizio_Prestito Date Not Null,
+    Data_Restituzione Date Not Null
+);
+
+Create Table Libro(
+    ISBN Int Primary Key,
+    Autore Varchar(50) Not Null,
+    Titolo Varchar(70) Not Null,
+    Anno_di_Pubblicazione Int Not Null
+);
+
+Create Table Categoria(
+    Id_Categoria Int Primary Key,
+    Nome Varchar(50) Not Null
+);
