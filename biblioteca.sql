829815106859e8939d45971729b9dc7148779fec
ahmed <159021809+crazymidoo@users.noreply.github.com>
2025-10-16 20:43:20 +0000

    Foreign Key

--- biblioteca.sql
+++ biblioteca.sql
@@ -9,14 +9,20 @@ Create Table Studente(
 Create Table Prestito(
     Id_Prestito Int Primary Key,
     Inizio_Prestito Date Not Null,
-    Data_Restituzione Date Not Null
+    Data_Restituzione Date Not Null,
+    Matricola Int Not Null,
+    ISBN Int Not Null,
+    Foreign Key(Matricola) References Studente(Matricola),
+    Foreign Key(ISBN) References Libro(ISBN)
 );
 
 Create Table Libro(
     ISBN Int Primary Key,
     Autore Varchar(50) Not Null,
     Titolo Varchar(70) Not Null,
-    Anno_di_Pubblicazione Int Not Null
+    Anno_di_Pubblicazione Int Not Null,
+    Id_Categoria Int Not Null,
+    Foreign Key(Id_Categoria) References Categoria(Id_Categoria)
 );
 
 Create Table Categoria(
